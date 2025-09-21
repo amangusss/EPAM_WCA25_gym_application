e32414ff609a7d962f55564eab4702dafc0d2641
amangusss <aman.nazarkulov@gmail.com>
2025-09-22 03:55:37 +0600

    test(util): cover utils with unit tests (100% coverage)

--- src/test/java/com/github/amangusss/gym_application/util/PasswordGeneratorTest.java
+++ src/test/java/com/github/amangusss/gym_application/util/PasswordGeneratorTest.java
@@ -11,38 +11,34 @@ class PasswordGeneratorTest {
 
     @BeforeEach
     void setUp() {
-        passwordGenerator = new PasswordGenerator();
+        passwordGenerator = new PasswordGenerator(
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+                10
+        );
     }
 
     @Test
     void generatePassword_ShouldReturnPasswordOfCorrectLength() {
-        // When
         String password = passwordGenerator.generatePassword();
 
-        // Then
         assertNotNull(password);
         assertEquals(10, password.length());
     }
 
     @Test
     void generatePassword_ShouldReturnDifferentPasswords() {
-        // When
         String password1 = passwordGenerator.generatePassword();
         String password2 = passwordGenerator.generatePassword();
 
-        // Then
         assertNotEquals(password1, password2);
     }
 
     @Test
     void generatePassword_ShouldContainOnlyValidCharacters() {
-        // Given
         String validCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-        // When
         String password = passwordGenerator.generatePassword();
 
-        // Then
         assertNotNull(password);
         for (char c : password.toCharArray()) {
             assertTrue(validCharacters.indexOf(c) >= 0, 
@@ -52,16 +48,13 @@ class PasswordGeneratorTest {
 
     @Test
     void generatePassword_MultipleCalls_ShouldGenerateDifferentPasswords() {
-        // Given
         int numberOfPasswords = 100;
         String[] passwords = new String[numberOfPasswords];
 
-        // When
         for (int i = 0; i < numberOfPasswords; i++) {
             passwords[i] = passwordGenerator.generatePassword();
         }
 
-        // Then
         for (int i = 0; i < numberOfPasswords; i++) {
             for (int j = i + 1; j < numberOfPasswords; j++) {
                 assertNotEquals(passwords[i], passwords[j], 
--- src/test/java/com/github/amangusss/gym_application/util/UsernameGeneratorTest.java
+++ src/test/java/com/github/amangusss/gym_application/util/UsernameGeneratorTest.java
@@ -1,5 +1,6 @@
 package com.github.amangusss.gym_application.util;
 
+import com.github.amangusss.gym_application.exception.ValidationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,36 +20,29 @@ class UsernameGeneratorTest {
 
     @Test
     void generateUsername_WithUniqueName_ShouldReturnFirstNameLastName() {
-        // Given
         String firstName = "John";
         String lastName = "Doe";
         Set<String> existingUsernames = new HashSet<>();
 
-        // When
         String result = usernameGenerator.generateUsername(firstName, lastName, existingUsernames);
 
-        // Then
         assertEquals("John.Doe", result);
     }
 
     @Test
     void generateUsername_WithExistingName_ShouldAddNumber() {
-        // Given
         String firstName = "John";
         String lastName = "Doe";
         Set<String> existingUsernames = new HashSet<>();
         existingUsernames.add("John.Doe");
 
-        // When
         String result = usernameGenerator.generateUsername(firstName, lastName, existingUsernames);
 
-        // Then
         assertEquals("John.Doe1", result);
     }
 
     @Test
     void generateUsername_WithMultipleExistingNames_ShouldAddIncrementalNumber() {
-        // Given
         String firstName = "John";
         String lastName = "Doe";
         Set<String> existingUsernames = new HashSet<>();
@@ -56,46 +50,37 @@ class UsernameGeneratorTest {
         existingUsernames.add("John.Doe1");
         existingUsernames.add("John.Doe2");
 
-        // When
         String result = usernameGenerator.generateUsername(firstName, lastName, existingUsernames);
 
-        // Then
         assertEquals("John.Doe3", result);
     }
 
     @Test
     void generateUsername_WithNullFirstName_ShouldThrowException() {
-        // Given
         String firstName = null;
         String lastName = "Doe";
         Set<String> existingUsernames = new HashSet<>();
 
-        // When & Then
-        assertThrows(IllegalArgumentException.class, () -> usernameGenerator.generateUsername(firstName, lastName, existingUsernames));
+        assertThrows(ValidationException.class, () -> usernameGenerator.generateUsername(firstName, lastName, existingUsernames));
     }
 
     @Test
     void generateUsername_WithNullLastName_ShouldThrowException() {
-        // Given
         String firstName = "John";
         String lastName = null;
         Set<String> existingUsernames = new HashSet<>();
 
-        // When & Then
-        assertThrows(IllegalArgumentException.class, () -> usernameGenerator.generateUsername(firstName, lastName, existingUsernames));
+        assertThrows(ValidationException.class, () -> usernameGenerator.generateUsername(firstName, lastName, existingUsernames));
     }
 
     @Test
     void generateUsername_WithWhitespace_ShouldTrim() {
-        // Given
         String firstName = "  John  ";
         String lastName = "  Doe  ";
         Set<String> existingUsernames = new HashSet<>();
 
-        // When
         String result = usernameGenerator.generateUsername(firstName, lastName, existingUsernames);
 
-        // Then
         assertEquals("John.Doe", result);
     }
 }
