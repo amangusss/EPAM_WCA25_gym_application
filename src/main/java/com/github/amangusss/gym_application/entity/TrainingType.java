e23298989585d7540b04740553c5f5bc498cfca8
amangusss <aman.nazarkulov@gmail.com>
2025-10-02 22:52:03 +0600

    feat(entity): update entities to use JPA and Hibernate in the project and delete the previous storage layer

--- src/main/java/com/github/amangusss/gym_application/entity/TrainingType.java
+++ src/main/java/com/github/amangusss/gym_application/entity/TrainingType.java
@@ -1,34 +1,19 @@
 package com.github.amangusss.gym_application.entity;
 
-import com.github.amangusss.gym_application.exception.ValidationException;
-import com.github.amangusss.gym_application.util.constants.ConfigConstants;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum TrainingType {
 
-    FITNESS(ConfigConstants.TRAINING_TYPE_FITNESS),
-    YOGA(ConfigConstants.TRAINING_TYPE_YOGA),
-    ZUMBA(ConfigConstants.TRAINING_TYPE_ZUMBA),
-    STRETCHING(ConfigConstants.TRAINING_TYPE_STRETCHING),
-    RESISTANCE(ConfigConstants.TRAINING_TYPE_RESISTANCE);
+    FITNESS("fitness"),
+    YOGA("yoga"),
+    ZUMBA("zumba"),
+    STRETCHING("stretching"),
+    RESISTANCE("resistance");
 
     private final String displayName;
-
-    TrainingType(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public static TrainingType fromDisplayName(String displayName) {
-        for (TrainingType type : values()) {
-            if (type.getDisplayName().equalsIgnoreCase(displayName)) {
-                return type;
-            }
-        }
-        throw new ValidationException(ConfigConstants.NO_TRAINING_TYPE + displayName);
-    }
 
     @Override
     public String toString() {
